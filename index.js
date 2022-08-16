369f9aa0494289f76e06bfbb0ba8524f103e4b40
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2022-08-16 10:59:28 +0300

    Port update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mysql.createConnection({
     database: 'test'
 })
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 //Konfigūracija nuotraukų priėmimui
 const storage = multer.diskStorage({
